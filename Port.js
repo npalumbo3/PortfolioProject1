fc42e818b552980b42d645158bbadd3955220303
npalumbo125 <npalumbo125@gmail.com>
2021-11-13 18:07:08 -0500

    Add random message 1

--- Port.js
+++ Port.js
@@ -5,6 +5,15 @@ rand1 = Math.floor(Math.random()*3);
 rand2 = Math.floor(Math.random()*3);
 rand3 = Math.floor(Math.random()*3);
 
+switch (rand1) {
+    case 0 :
 
+    console.log("What up fam");
+    break;
+
+    default:
+        console.log('What are you doing in my swamp');
+
+}
 
 
